548590af8cb4f57ef7f45b048679ce940a0d0597
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2022-05-29 18:45:35 +0100

    another seo

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -1,33 +1,22 @@
 import Head from "next/head";
-import config from "./config";
-import { useRouter } from "next/router";
 
-const SEO = ({ title, image, description }) => {
-  const { pathname } = useRouter();
-
-  const {
-    originalTitle,
-    originalDescription,
-    siteName,
-    social: { twitter },
-    currentURL,
-    originalImage,
-    keywords,
-  } = config;
-
+const SEO = () => {
   return (
     <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta charSet='utf-8' />
-      <title>{originalTitle}</title>
-      <meta name='description' content={originalDescription} />
-      <meta name='keywords' content={keywords} />
+      <title>Astroneir Design - Graphic Designer</title>
+      <meta name='description' content='Graphic Designer' />
+      <meta
+        name='keywords'
+        content=' "Graphic Design, Colours, Illustration, Web Design, Video Editing, Anestis Neiros, Astroneir Design, Branding, Music Posters, Movie Posters, Packaging, Movie Trailers, T Shirts"'
+      />
       <link rel='icon' href='/favicon.ico' />
-      <meta name='image' content={originalImage} key='ogtitle' />
-      <meta property='og:title' content={originalTitle} key='ogtitle' />
+      <meta name='image' content='/assets/website.png' key='ogtitle' />
+      <meta property='og:title' content='Astroneir Design' key='ogtitle' />
       <meta
         property='og:description'
-        content={originalDescription}
+        content='Creative Graphic Designer, fluent in Illustrator and Photoshop, with talent for developing unique custom artworks. Highly artistic, always on task with innovative approach to ideas and concept development.'
         key='ogdesc'
       />
       <meta
@@ -35,21 +24,21 @@ const SEO = ({ title, image, description }) => {
         content='summary_large_image'
         key='twcard'
       />
-      <meta name='twitter:creator' content={twitter} key='twhandle' />
-      <meta name='twitter:title' content={originalTitle} key='twtitle' />
+      <meta name='twitter:creator' content='Gioumourtzina' key='twhandle' />
+      <meta name='twitter:title' content='Astroneir Design' key='twtitle' />
       <meta
         name='twitter:description'
-        content={originalDescription}
+        content='Creative Graphic Designer, fluent in Illustrator and Photoshop, with talent for developing unique custom artworks. Highly artistic, always on task with innovative approach to ideas and concept development.'
         key='twdescription'
       />
-      <meta name='twitter:image' content={originalImage} key='twimage' />
+      <meta name='twitter:image' content='/assets/website.png' key='twimage' />
+      <meta property='og:url' content='www.astroneir-design.com' key='ogurl' />
+      <meta property='og:image' content='/assets/website.png' key='ogimage' />
       <meta
-        property='og:url'
-        content={`www.astroneir-design.com`}
-        key='ogurl'
+        property='og:site_name'
+        content='Astroneir Design'
+        key='ogsitename'
       />
-      <meta property='og:image' content={originalImage} key='ogimage' />
-      <meta property='og:site_name' content={siteName} key='ogsitename' />
     </Head>
   );
 };
