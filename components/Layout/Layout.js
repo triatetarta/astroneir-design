67909a53393dedca1cf82596e666c9a2330280ee
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2022-05-29 15:01:09 +0100

    new gradient

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import { Header } from "./Header";
-import Granim from "granim";
+// import Granim from "granim";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Bio } from "../Bio";
 import { Contact } from "../Contact";
 import { AnimatePresence } from "framer-motion";
 import { useRouter } from "next/router";
+import { Gradient } from "../../Gradient";
 
 const Layout = ({ children }) => {
   const [isWorks, setIsWorks] = useState(false);
@@ -15,42 +16,49 @@ const Layout = ({ children }) => {
 
   const router = useRouter();
 
+  const gradient = new Gradient();
+
+  useEffect(() => {
+    gradient.initGradient("#gradient-canvas");
+  }, []);
+
   useEffect(() => {
     if (router.pathname !== "/works" && router.pathname !== "/music") return;
 
     setIsWorks(true);
   }, [router]);
 
-  useEffect(() => {
-    if (track === undefined) return;
+  // useEffect(() => {
+  //   if (track === undefined) return;
 
-    new Granim({
-      element: "#logo-canvas",
-      direction: "diagonal",
-      opacity: [1, 1],
-      states: {
-        "default-state": {
-          gradients: [
-            [
-              { color: track.colors.first, pos: 0 },
-              { color: track.colors.middle, pos: 0.2 },
-              { color: track.colors.last, pos: 0.75 },
-            ],
-            [
-              { color: track.colors.first, pos: 0 },
-              { color: track.colors.middle, pos: 0.2 },
-              { color: track.colors.last, pos: 0.75 },
-            ],
-          ],
-          transitionSpeed: 10000,
-        },
-      },
-    });
-  }, [track]);
+  //   new Granim({
+  //     element: "#logo-canvas",
+  //     direction: "diagonal",
+  //     opacity: [1, 1],
+  //     states: {
+  //       "default-state": {
+  //         gradients: [
+  //           [
+  //             { color: track.colors.first, pos: 0 },
+  //             { color: track.colors.middle, pos: 0.2 },
+  //             { color: track.colors.last, pos: 0.75 },
+  //           ],
+  //           [
+  //             { color: track.colors.first, pos: 0 },
+  //             { color: track.colors.middle, pos: 0.2 },
+  //             { color: track.colors.last, pos: 0.75 },
+  //           ],
+  //         ],
+  //         transitionSpeed: 10000,
+  //       },
+  //     },
+  //   });
+  // }, [track]);
 
   return (
     <>
-      <canvas id='logo-canvas'></canvas>
+      {/* <canvas id='logo-canvas'></canvas> */}
+      <canvas id='gradient-canvas' data-transition-in />
       <div className='noise-bg overflow-hidden'></div>
       <Header />
       <div
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import "../styles/globals.css";
+import "../styles/gradient.css";
 import { Provider } from "react-redux";
 import { store } from "../configureStore";
 import { Cursor } from "../components/Cursor";
