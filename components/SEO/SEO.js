902b993759080e796f927811e93b1c85956540f8
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2022-05-29 19:39:00 +0100

    another one

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -5,37 +5,23 @@ const SEO = () => {
     <Head>
       <meta name='viewport' content='width=device-width, initial-scale=1' />
       <meta charSet='utf-8' />
-      <title>Astroneir Design - Graphic Designer</title>
+      <title>Astroneir Design</title>
       <meta name='description' content='Graphic Designer' />
-      <meta
-        name='keywords'
-        content=' "Graphic Design, Colours, Illustration, Web Design, Video Editing, Anestis Neiros, Astroneir Design, Branding, Music Posters, Movie Posters, Packaging, Movie Trailers, T Shirts"'
-      />
-      <link rel='icon' href='/favicon.ico' />
-      {/* <meta name='image' content='https://i.imgur.com/dWXIOhh.png' /> */}
 
-      {/* Twitter */}
-      <meta name='twitter:card' content='summary_large_image' key='twcard' />
-      <meta name='twitter:creator' content='Gioumourtzina' key='twhandle' />
+      <meta itemprop='name' content='Astroneir Design' />
+      <meta itemprop='description' content='Graphic Designer' />
+      <meta itemprop='image' content='https://i.imgur.com/dWXIOhh.png' />
 
-      {/* Open Graph */}
-      <meta property='og:url' content='www.astroneir-design.com' key='ogurl' />
-      <meta
-        property='og:image'
-        content='https://i.imgur.com/dWXIOhh.png'
-        key='ogimage'
-      />
-      <meta
-        property='og:site_name'
-        content='Astroneir Design'
-        key='ogsitename'
-      />
-      <meta property='og:title' content='Graphic Designer' key='ogtitle' />
-      <meta
-        property='og:description'
-        content='Creative Graphic Designer, fluent in Illustrator and Photoshop, with talent for developing unique custom artworks. Highly artistic, always on task with innovative approach to ideas and concept development.'
-        key='ogdesc'
-      />
+      <meta property='og:url' content='https://www.astroneir-design.com' />
+      <meta property='og:type' content='website' />
+      <meta property='og:title' content='Astroneir Design' />
+      <meta property='og:description' content='Graphic Designer' />
+      <meta property='og:image' content='https://i.imgur.com/dWXIOhh.png' />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content='Astroneir Design' />
+      <meta name='twitter:description' content='Graphic Designer' />
+      <meta name='twitter:image' content='https://i.imgur.com/dWXIOhh.png' />
     </Head>
   );
 };
