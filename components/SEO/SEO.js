b69d032512994e0b1b2f7a617fee9b5343bc52ad
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2022-05-29 19:27:54 +0100

    another try

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -12,23 +12,30 @@ const SEO = () => {
         content=' "Graphic Design, Colours, Illustration, Web Design, Video Editing, Anestis Neiros, Astroneir Design, Branding, Music Posters, Movie Posters, Packaging, Movie Trailers, T Shirts"'
       />
       <link rel='icon' href='/favicon.ico' />
-      <meta name='image' content='https://i.imgur.com/dWXIOhh.png' />
-      <meta name='twitter:card' content='summary_large_image' />
-      <meta name='twitter:title' content='Astroneir Design' />
-      <meta
-        name='twitter:description'
-        content='Creative Graphic Designer, fluent in Illustrator and Photoshop, with talent for developing unique custom artworks.'
-      />
-      <meta name='twitter:creator' content='@Gioumourtzina' />
-      <meta name='twitter:image' content='https://i.imgur.com/dWXIOhh.png' />
+      {/* <meta name='image' content='https://i.imgur.com/dWXIOhh.png' /> */}
 
-      <meta property='og:title' content='Astroneir Design - Graphic Designer' />
+      {/* Twitter */}
+      <meta name='twitter:card' content='summary_large_image' key='twcard' />
+      <meta name='twitter:creator' content='Gioumourtzina' key='twhandle' />
+
+      {/* Open Graph */}
+      <meta property='og:url' content='www.astroneir-design.com' key='ogurl' />
+      <meta
+        property='og:image'
+        content='https://i.imgur.com/dWXIOhh.png'
+        key='ogimage'
+      />
+      <meta
+        property='og:site_name'
+        content='Astroneir Design'
+        key='ogsitename'
+      />
+      <meta property='og:title' content='Graphic Designer' key='ogtitle' />
       <meta
         property='og:description'
         content='Creative Graphic Designer, fluent in Illustrator and Photoshop, with talent for developing unique custom artworks. Highly artistic, always on task with innovative approach to ideas and concept development.'
+        key='ogdesc'
       />
-      <meta property='og:image' content='https://i.imgur.com/dWXIOhh.png' />
-      <meta property='og:type' content='website' />
     </Head>
   );
 };
