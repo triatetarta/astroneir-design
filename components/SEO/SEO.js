f35f95892b77f5db95233c7a55d5f07485a4b598
Dimitrios Chatziioannou <dimchatziioannou@gmail.com>
2022-05-29 18:50:33 +0100

    changes

--- components/SEO/SEO.js
+++ components/SEO/SEO.js
@@ -12,8 +12,16 @@ const SEO = () => {
         content=' "Graphic Design, Colours, Illustration, Web Design, Video Editing, Anestis Neiros, Astroneir Design, Branding, Music Posters, Movie Posters, Packaging, Movie Trailers, T Shirts"'
       />
       <link rel='icon' href='/favicon.ico' />
-      <meta name='image' content='/assets/website.png' key='ogtitle' />
-      <meta property='og:title' content='Astroneir Design' key='ogtitle' />
+      <meta
+        name='image'
+        content='https://i.imgur.com/dWXIOhh.png'
+        key='ogtitle'
+      />
+      <meta
+        property='og:title'
+        content='Astroneir Design - Graphic Designer'
+        key='ogtitle'
+      />
       <meta
         property='og:description'
         content='Creative Graphic Designer, fluent in Illustrator and Photoshop, with talent for developing unique custom artworks. Highly artistic, always on task with innovative approach to ideas and concept development.'
@@ -31,9 +39,17 @@ const SEO = () => {
         content='Creative Graphic Designer, fluent in Illustrator and Photoshop, with talent for developing unique custom artworks. Highly artistic, always on task with innovative approach to ideas and concept development.'
         key='twdescription'
       />
-      <meta name='twitter:image' content='/assets/website.png' key='twimage' />
+      <meta
+        name='twitter:image'
+        content='https://i.imgur.com/dWXIOhh.png'
+        key='twimage'
+      />
       <meta property='og:url' content='www.astroneir-design.com' key='ogurl' />
-      <meta property='og:image' content='/assets/website.png' key='ogimage' />
+      <meta
+        property='og:image'
+        content='https://i.imgur.com/dWXIOhh.png'
+        key='ogimage'
+      />
       <meta
         property='og:site_name'
         content='Astroneir Design'
